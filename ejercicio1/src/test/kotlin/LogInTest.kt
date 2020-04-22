31aad9379e34e2af78f86e030ecd4f75afd9b3a0
Esteban Franco <LeandroFranco97@outlook.es>
2020-04-21 21:20:23 -0300

    agrego test para verificar que autenticar funciona

---
+++ ejercicio1/src/test/kotlin/LogInTest.kt
@@ -0,0 +1,26 @@
+package ejercicio1
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class LogInTest {
+    var sistema:Sistema = Sistema(mutableListOf())
+    var user: User = User("lean","1234")
+    var login: Login = Login()
+    @Test
+
+    fun sistemaContieneUser(){
+        sistema.agregarUsuario(user)
+        assertTrue((sistema.usuarios).contains(user))
+    }
+
+    @Test
+    fun autenticarFunciona(){
+        login.setearName("lean")
+        login.setearPass("1234")
+        sistema.agregarUsuario(user)
+        assertTrue(login.autenticar())
+    }
+
+
+}
